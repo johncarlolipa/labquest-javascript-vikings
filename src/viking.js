57c81085f066a4515a064586b1bab87a7593ed78
unknown <johncarlolipa@gmail.com>
2023-11-02 22:38:14 +0800

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -12,20 +12,51 @@ class Soldier {
   }
 }
 
-const soldier1 = new Soldier(55, 70)
-console.log(soldier1.receiveDamage());
+const soldier1 = new Soldier(55, 70);
+console.log(soldier1.receiveDamage(20));
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in the act of combat`;
+    }
+  }
+  battleCry() {
+    return `${this.name} "Owns you all"`;
+  }
+}
+
+let viking = new Viking('Odin', 50, 60);
+console.log(viking.battleCry());
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage){
+    this.health -= damage;
+    if(this.health > 0){
+      return `A Saxon has received ${damage} points of damage.`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
+
+let saxon = new Saxon(50, 60);
+console.log(saxon.receiveDamage(20));
 
 // War
 class War {}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
-if (typeof module !== 'undefined') {
+if (typeof module !== "undefined") {
   module.exports = { Soldier, Viking, Saxon, War };
 }
